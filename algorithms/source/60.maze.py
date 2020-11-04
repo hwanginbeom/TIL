5bac6366cd15259937689ae8ce9abaabe08bfc7a
hwanginbeom <rydn2002@gmail.com>
2020-11-05 01:21:31 +0900

    20.11.05 | algorithms_BFS |60.maze

--- algorithms/source/60.maze.py
+++ algorithms/source/60.maze.py
@@ -1,3 +1,33 @@
-from collections import deque #BFS 메서드 정의 def bfs(graph,start,visited): queue = deque([start]) # 현재 노드를 방문처리 visited[start] = True # 큐가 빌때까지 반복 while queue: # 큐에서 하나의 원소를 뽑아 출력하기 v = queue.popleft() print(v,end=' ') # 아직 방문하지 않은 인접한 원소들을 큐에 삽입 for i in graph[v]: if not visited[i]: queue.append(i) visited[i] = True # 각 노드가 연결된 정보를 표현(2차원 리스트) graph =[ [], [2,3,8], [1,7], [1,4,5], [3,5], [3,4], [7], [2,6,8], [1,7] ] #각 노드가 방문된 정보를 표현(1차원 리스트) visited = [False]*9 #정의된 BFS 함수 호출 bfs(graph, 1, visited)
 
-출처: https://continuous-development.tistory.com/175?category=736684 [나무늘보의 개발 블로그]
+from collections import deque
+
+N,M  = map(int, input().split())
+
+maze = []
+for i in range(N):
+    maze.append(list(map(int, input())))
+
+dx, dy =  [-1, 1, 0, 0],  [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if (nx < 0) or (ny < 0) or (nx >= N) or (ny >= M):
+                continue
+            if maze[nx][ny] == 0:
+                continue
+            if maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1
+                queue.append((nx, ny))
+    return maze[N-1][M-1]
+
+
+print(bfs(0, 0))
+
