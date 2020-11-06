f7e28f75ed860fee1932f6695d265341e3d60d90
hwanginbeom <rydn2002@gmail.com>
2020-11-06 17:19:52 +0900

    20.11.06 | algorithms | sort

--- algorithms/source/test.py
+++ algorithms/source/test.py
@@ -1,15 +1,36 @@
-array=[]
-value = str(input())
-for z in range(0, len(value)):
-    array.append( int(value[z]))
-    
-for i in range(len(array)):
-    min_index = i  # 가장 작은 원소의 인덱스
-    for j in range(i + 1, len(array)):
-        if array[min_index] > array[j]:
-            min_index = j
-        array[i], array[min_index] = array[min_index], array[i]  # 스와프
-result=''
-for i in array:
-    result += str(i)
-print(result)
+import sys
+from collections import Counter
+value = int(sys.stdin.readline())
+value_list = [0] * value
+print(value)
+number = []
+for i in range(value):
+    number.append(int(sys.stdin.readline()))
+
+
+def function1(number):
+    result = 0
+    for i in number:
+        result += i
+    result = result / len(number)
+    return round(result, 2)
+
+def function2(number):
+    result = len(number) // 2
+    return number[result]
+
+def function3(number):
+    cnt = Counter(number)
+    result = cnt.most_common(1)[0][0]
+    return result
+
+
+# def function2(number):
+#     dict
+#     result = len(number) // 2
+#     return number[result]
+
+
+print(function1(number))
+print(function2(number))
+print(function3(number))
