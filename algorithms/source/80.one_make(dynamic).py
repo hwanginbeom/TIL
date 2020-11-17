f2e4f37f8784c84f86eebd94e9646cb974e08233
hwanginbeom <rydn2002@gmail.com>
2020-11-17 17:32:10 +0900

    20.11.17 | algorithm-source | 80.one_make

--- algorithms/source/80.one_make(dynamic).py
+++ algorithms/source/80.one_make(dynamic).py
@@ -35,4 +35,7 @@ for i in range(2, n + 1):
     if i % 3 == 0 and dp[i] > dp[i // 3] + 1:
         dp[i] = dp[i // 3] + 1
 
+    if i % 5 == 0 and dp[i] > dp[i // 5] + 1:
+        dp[i] = dp[i // 5] + 1
+
 print(dp[n])
