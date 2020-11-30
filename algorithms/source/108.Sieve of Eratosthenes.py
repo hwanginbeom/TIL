db97e845e547bb20629e0a223f09972742a2c671
hwanginbeom <rydn2002@gmail.com>
2020-12-01 03:50:46 +0900

    20.11.30 | 108.Sieve of Eratoshenes

---
+++ algorithms/source/108.Sieve of Eratosthenes.py
@@ -0,0 +1,18 @@
+#2960번 - 에라토스테네스의 체
+import math
+
+n = 1000
+
+array = [True for i in range(n + 1)]
+
+for i in range(2, int(math.sqrt(n) + 1)):
+    if array[i] == True:
+        j = 2
+        while i * j <= n:
+            array[i * j] = False
+            print(i * j)
+            j += 1
+
+for i in range(2, n + 1):
+    if array[i]:
+        print(i, end=' ')
