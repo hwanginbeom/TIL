468da5986f1822303b262b4eb2f6547927e02017
hwanginbeom <rydn2002@gmail.com>
2020-12-03 23:28:06 +0900

    20.12.03 | source | 113.coin(greedy)

---
+++ algorithms/source/115.coin(greedy).py
@@ -0,0 +1,15 @@
+# 11047 동전 문제
+a, b = map(int, input().split())
+count = 0
+array = []
+
+for i in range(a):
+    c = int(input())
+    array.append(c)
+
+array.reverse()
+for coin in array:
+    count += b // coin
+    b %= coin
+
+print(count)
