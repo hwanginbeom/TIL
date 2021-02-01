c6f4b34b5e1b19c79e17c160587035282e3bca9d
hwanginbeom <rydn2002@gmail.com>
2021-02-01 21:24:09 +0900

    21.01.01 | dart

---
+++ algorithms/source/sss.py
@@ -0,0 +1,22 @@
+import re
+
+
+def solution(dartResult):
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    dart = p.findall(dartResult)
+    print(dart)
+    for i in range(len(dart)):                # 정규표현식으로 갈무리된 문자열들
+        if dart[i][2] == '*' and i > 0:
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+
+    answer = sum(dart)
+    print(answer)
+    return answer
+
+
+dartResult = "1S2D*3T"
+solution(dartResult)
