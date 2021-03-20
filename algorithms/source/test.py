ee71c3099cdc215331474a544262014fe3abbcb9
hwanginbeom <rydn2002@gmail.com>
2021-03-20 23:09:46 +0900

    modified

--- algorithms/source/test.py
+++ algorithms/source/test.py
@@ -1,24 +1,25 @@
-count = 0
-def dfs(graph, v, visited):
-    visited[v] = True
-    global count
-    count += 1
-    for i in graph[v]:
-        if not visited[i]:
-            dfs(graph, i, visited)
+def dijkstra(start) :
+    # 최단 거리 테이블을 모두 무한 => m+1 로 초기화
+    distance = [m + 1] * (n + 1)  # m+1이 최대가 되는것
+    q = []
 
-computer = int(input())
-connect_num = int(input())
-node = []
+    # 시작 노드로 가기 위한 최단 거리는 0으로 설정하여 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
 
-for i in range(0,computer+1):
-    node.append([])
-for i in range(0,connect_num):
-    node_num, connect = list(map(int, input().split()))
-    node[node_num].append(connect)
-    node[connect].append(node_num)
+    # 큐가 비어있지 않다면
+    while q:
+        # 가장 최단 거리가 짧은 노드에 대한 정보를 꺼내기
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
 
-visited = [False] * (computer+1)
-dfs(node, 1, visited)
-count = count-1
-print(count)
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+
+            # 현재 노드를 거쳐, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+    return distance
